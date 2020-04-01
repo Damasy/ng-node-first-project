4625c0425a391f54d3c6f21dd07eb3ed46612634
Ahmed Hussein <7925@skp.dev>
2020-04-01 14:01:50 +0200

    install dependecies and build server imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'projects', component: ProjectsComponent, pathMatch: 'full'},
+      { path: 'contact', component: ContactComponent, pathMatch: 'full'},
+
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+// import {  } from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -15,8 +21,11 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({appId: 'ng-node-app'}),
+    // MaterialModule,
+    AppRoutingModule,
+    // HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
